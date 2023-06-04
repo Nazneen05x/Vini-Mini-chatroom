8583199d84ed4219e9f148f4284c9138fc910465
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2023-06-04 15:51:05 +0200

    Done

--- public/script.js
+++ public/script.js
@@ -1,6 +1,12 @@
 let ioServer = io()
 let messages = document.querySelector('section ul')
 let input = document.querySelector('input')
+let Gebruikersnaam = document.querySelector('#Gebruikersnaam')
+let Bericht = document.querySelector('#Bericht')
+
+
+
+
 
 // Luister naar het submit event
 document.querySelector('form').addEventListener('submit', (event) => {
@@ -9,17 +15,26 @@ document.querySelector('form').addEventListener('submit', (event) => {
   // Als er überhaupt iets getypt is
   if (input.value) {
     // Stuur het bericht naar de server
-    ioServer.emit('message', input.value)
+    ioServer.emit('message',{ 
+      Gebruikersnaam: Gebruikersnaam.value,
+      Bericht: Bericht.value
+    })
 
     // Leeg het form field
-    input.value = ''
+    Bericht.value = ''
   }
 })
 
+
+
+
 // Luister naar berichten van de server
 ioServer.on('message', (message) => {
-  addMessage(message)
+  addMessage(`${message.Gebruikersnaam}: ${message.Bericht}`)
+
 })
+
+
 
 /**
  * Impure function that appends a new li item holding the passed message to the
